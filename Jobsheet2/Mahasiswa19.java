7b9a5e2bc1da70651c6e3c8f5ef8ef56cdab42c5
naufalazmiaqil <incoldtown@gmail.com>
2026-02-20 14:42:28 +0700

    Percobaan 1

---
+++ Jobsheet2/Mahasiswa19.java
@@ -0,0 +1,40 @@
+package Jobsheet2;
+
+public class Mahasiswa19 {
+
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    void tampilInformasi() {
+        System.out.println("NIM   : " + nim);
+        System.out.println("Nama  : " + nama);
+        System.out.println("Kelas : " + kelas);
+        System.out.println("IPK   : " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid (0.0 - 4.0)");
+        }
+    }
+
+    String nilaiKinerja() {
+            if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        } 
+    } 
+}
