0a1ecaafef85f67b349fdc3300fef34ebb7bdd1f
naufalazmiaqil <incoldtown@gmail.com>
2026-02-20 15:24:31 +0700

    Percobaan 3 "mahasiswa main"

--- Jobsheet2/MahasiswaMain19.java
+++ Jobsheet2/MahasiswaMain19.java
@@ -14,6 +14,10 @@ public class MahasiswaMain19 {
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilInformasi();
+
+        Mahasiswa19 mhs2 = new Mahasiswa19("Annisa Nabila", "2141720160", 3.25, "TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilInformasi();
     }
 }
 
