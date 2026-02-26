56b6ff4d1a7c36ad209fd21910859ca30bca8def
naufalazmiaqil <incoldtown@gmail.com>
2026-02-26 10:04:33 +0700

    Percobaan 2 modifikasi 1

--- Jobsheet3/Mahasiswa19.java
+++ Jobsheet3/Mahasiswa19.java
@@ -5,4 +5,11 @@ public class Mahasiswa19 {
     public String nama;
     public String kelas;
     public float ipk;
+
+    public void cetakinfo(){
+        System.out.println("NIM     : " + nim);
+        System.out.println("Nama    : " + nama);
+        System.out.println("Kelas   : " + kelas);
+        System.out.println("IPK     : " + ipk);
+    }
 }
